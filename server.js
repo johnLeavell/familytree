86197fcf28ab24de674fa3ef8e222130f2cf4b66
John Leavell <johnleavell@gmail.com>
2022-07-17 21:56:49 -0500

    connected to mongodb compas

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 3000;
 const routes = require("./routes");
 const connectDB = require("./config/config.js");
 const mongoose = require("mongoose");
-const { notFound, errorHandler } = require("./utils/errorHandler");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -18,8 +17,7 @@ mongoose.Promise = global.Promise;
 connectDB();
 
 app.use("/", routes);
-app.use(notFound);
-app.use(errorHandler);
+
 app.listen(port, () => {
   console.log(`Node server is listening on port ${port}`);
 });
